9b2a976aed3acaf806ef179e2e3f2c51f8942260
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-26 17:30:08 +0530

    test

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,7 +1,19 @@
+## Simple variable
+
 variable "fruit_name" {
   default = "apple"
 }
 
-output "message" {
+output "message-1" {
   value = var.fruit_name
+}
+
+## List variable
+
+variable "fruit_list" {
+  default = ["apple", "banana"]
+}
+
+output "message-2" {
+  value = var.fruit_list
 }
