e80591a8de665098624b4bb979db0d8b7646aa81
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 15:45:04 +0530

    test

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,17 +2,17 @@ variable "components" {
   default = ["catalogue", "user", "frontend", "mongodb"]
 }
 
-# resource "aws_instance" "instance" {
-#   count         = length(var.components)
-#   ami           = var.ami
-#   instance_type = var.instance_type
-#   vpc_security_group_ids = var.vpc_security_group_ids
-#
-#   tags = {
-#     Name = element(var.components, count.index)
-#     # Name = var.components[count.index]
-#   }
-# }
+resource "aws_instance" "instance" {
+  count         = length(var.components)
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = element(var.components, count.index)
+    # Name = var.components[count.index]
+  }
+}
 
 variable "ami" {
   default = "ami-0b4f379183e5706b9"
