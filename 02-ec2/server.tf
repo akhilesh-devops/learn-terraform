2fb4ab7d6007490cb4928f4b17270c71d7c9eb73
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 11:14:50 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z0345275C3S6UDSOR4CU"
+  name    = "frontend.vinithaws.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
 }
