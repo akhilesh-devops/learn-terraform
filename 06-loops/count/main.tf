159626e1fc7f28b95fc417866392b2484325a3a7
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 13:14:20 +0530

    test

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -9,7 +9,8 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = element(var.components, count.index)
+    # Name = element(var.components, count.index)
+    Name = var.components[count.index]
   }
 }
 
