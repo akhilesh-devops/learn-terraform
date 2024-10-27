9918e0f55935296c85cd395dcfabaf57288aed9d
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 15:59:52 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = lookup(each.key, null)
+    Name = lookup(each.key, "null")
   }
 }
 
