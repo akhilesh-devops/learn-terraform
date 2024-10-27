e079c0087a6b5c67e8065c10adbe70062f2b8f5a
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 15:57:01 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,20 +1,21 @@
 resource "aws_instance" "instance" {
+  for_each      = var.components
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = "frontend"
+    Name = lookup(each.key, null)
   }
 }
 
-resource "aws_route53_record" "record" {
-  zone_id = var.zone_id
-  name    = "frontend.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance.private_ip]
-}
+# resource "aws_route53_record" "record" {
+#   zone_id = var.zone_id
+#   name    = "frontend.vinithaws.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.instance.private_ip]
+# }
 
 variable "ami" {
   default = "ami-0b4f379183e5706b9"
@@ -30,4 +31,19 @@ variable "vpc_security_group_ids" {
 
 variable "zone_id" {
   default = "Z0345275C3S6UDSOR4CU"
+}
+
+variable "components" {
+  default = {
+    frontend = {}
+    mongodb = {}
+    catalogue = {}
+    redis = {}
+    user = {}
+    cart = {}
+    mysql = {}
+    shipping = {}
+    rabbitmq = {}
+    payment = {}
+  }
 }
