a56c9bf61cbd4863d3d0f0a60df501424e22ff59
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-27 12:33:42 +0530

    test

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,5 +1,9 @@
-data "aws_ami" "example" {
+data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "centos-*"
   owners           = ["973714476881"]
+}
+
+output "ami" {
+  value = data.aws_ami.ami
 }
