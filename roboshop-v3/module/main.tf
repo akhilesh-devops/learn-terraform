e59bf743c76e7c4cc421a5fe8fd61b1c525695db
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-01 12:05:10 +0530

    test

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instances" {
 
 resource "aws_route53_record" "frontend" {
   zone_id    = var.zone_id
-  name       = "${var.name}-dev.akhildevops.online"
+  name       = "${var.name}.akhildevops.online"
   type       = "A"
   ttl        = 30
   records    = [aws_instance.instances.private_ip]
