bbd1b09c548c05fe60a7f7587f0eaac1470df05c
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-01 11:14:06 +0530

    test

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instance" {
+resource "aws_instance" "frontend" {
   ami                    = "ami-0b4f379183e5706b9"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
   instance_type          = "t2.micro"
@@ -8,10 +8,28 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "frontend" {
   zone_id = "Z0345275C3S6UDSOR4CU"
   name    = "frontend.akhildevops.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instance.id]
+  records = [aws_instance.frontend.id]
+}
+
+resource "aws_instance" "mongodb" {
+  ami                    = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0345275C3S6UDSOR4CU"
+  name    = "frontend.akhildevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.id]
 }
