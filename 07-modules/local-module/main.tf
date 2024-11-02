c9d797e84ab70c587a410c1ff725ec3e4c5d29e7
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-02 13:50:55 +0530

    test

--- 07-modules/local-module/main.tf
+++ 07-modules/local-module/main.tf
@@ -4,6 +4,16 @@ data "aws_ami" "ami" {
   owners           = ["973714476881"]
 }
 
-output "test" {
-  value = data.aws_ami.ami.id
+resource "aws_instance" "instance" {
+  vpc_security_group_ids  = var.vpc_security_group_ids
+  instance_type           = var.instance_type
+  ami                     = data.aws_ami.ami.id
+
+  tags = {
+    Name = var.name
+  }
 }
+
+variable "name" {}
+variable "instance_type" {}
+variable "vpc_security_group_ids" {}
--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -1,7 +1,18 @@
 module "test" {
   source = "./local-module"
+  name   = var.name
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 }
 
-output "test" {
-  value = module.test
+
+
+variable "name" {
+  default = ["frontend"]
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-0ad8ec6873fafd140"]
 }
