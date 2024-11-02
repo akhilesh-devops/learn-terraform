242578fda298d536a71b4c7aa9cd2e249c9d090f
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-02 11:38:08 +0530

    test

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,6 +1,7 @@
 module "components" {
-  source                  = "./module"
   for_each                = var.components
+  source                  = "./module"
+
   vpc_security_group_ids  = var.vpc_security_group_ids
   zone_id                 = var.zone_id
   instance_type           = each.value["instance_type"]
