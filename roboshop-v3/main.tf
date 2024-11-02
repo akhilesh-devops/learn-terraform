3fef6ac825bbf0770e9c94926d8ee66760490fd1
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-02 11:32:51 +0530

    test

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -11,16 +11,16 @@ module "components" {
 
 components = {
   default = {
-    frontend   = { name = "frontend", type = "t2.micro" }
-    mongodb    = { name = "mongodb", type = "t2.micro" }
-    catalogue  = { name = "catalogue", type = "t2.micro" }
-    redis      = { name = "redis", type = "t2.micro" }
-    user       = { name = "user", type = "t2.micro" }
-    cart       = { name = "cart", type = "t2.micro" }
-    mysql      = { name = "mysql", type = "t2.micro" }
-    shipping   = { name = "shipping", type = "t2.micro" }
-    rabbitmq   = { name = "rabbitmq", type = "t2.micro" }
-    payment    = { name = "payment", type = "t2.micro" }
+    frontend   = { name = "frontend", instance_type = "t2.micro" }
+    mongodb    = { name = "mongodb", instance_type = "t2.micro" }
+    catalogue  = { name = "catalogue", instance_type = "t2.micro" }
+    redis      = { name = "redis", instance_type = "t2.micro" }
+    user       = { name = "user", instance_type = "t2.micro" }
+    cart       = { name = "cart", instance_type = "t2.micro" }
+    mysql      = { name = "mysql", instance_type = "t2.micro" }
+    shipping   = { name = "shipping", instance_type = "t2.micro" }
+    rabbitmq   = { name = "rabbitmq", instance_type = "t2.micro" }
+    payment    = { name = "payment", instance_type = "t2.micro" }
   }
 }
 
