78012e44dcdd64b2b457018fa002088e7c5622f8
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 13:29:17 +0530

    test

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -19,9 +19,9 @@ resource "aws_instance" "instances" {
   for_each               = var.components
   ami                    = "ami-0b4f379183e5706b9"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
-  instance_type          = each.value["instance_type"]
+  instance_type          = lookup(each.value["instance_type"], null)
 
   tags = {
-    Name = each.value["name"]
+    Name = lookup(each.value["name"], null)
   }
 }
