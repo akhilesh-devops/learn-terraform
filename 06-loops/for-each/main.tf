cc3d09515e2a6b9fecba73c7b387a611203d94dc
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 13:28:18 +0530

    test

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,14 +1,27 @@
 variable "components" {
   default = {
-    frontend = { name = "frontend_sg" }
-    cart     = { name = "cart_sg" }
-    mongodb  = { name = "mongodb_sg" }
-    redis    = { name = "redis_sg"}
-    catalogue = { name = "catalogue_sg" }
+    frontend = {
+      name          = "frontend-dev"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = "mongodb-dev"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue-dev"
+      instance_type = "t2.micro"
+    }
   }
 }
 
-resource "aws_security_group" "allow-all" {
-  for_each = var.components
-  name  = lookup(each.value, "name", null)
+resource "aws_instance" "instances" {
+  for_each               = var.components
+  ami                    = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  instance_type          = each.value["instance_type"]
+
+  tags = {
+    Name = each.value["name"]
+  }
 }
