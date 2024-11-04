62649bb0d55dc4bf4d2b8bdf596a35aab22a08d4
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 12:13:39 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,51 +1,51 @@
-resource "aws_instance" "instance" {
-  for_each      = var.components
-  ami           = var.ami
-  instance_type = lookup(each.value, "type", null)
-  vpc_security_group_ids = var.vpc_security_group_ids
+
+# output "aws_instance" {
+#   value = aws_instance.instance
+# }
+#
+# resource "aws_route53_record" "record" {
+#   for_each      = var.components
+#   zone_id       = var.zone_id
+#   name          = "${lookup(each.value, "name", null)}.vinithaws.online"
+#   type          = "A"
+#   ttl           = 30
+#   records       = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
+# }
+#
+# variable "ami" {
+#   default = "ami-0b4f379183e5706b9"
+# }
+#
+#
+# variable "vpc_security_group_ids" {
+#   default = ["sg-0ad8ec6873fafd140"]
+# }
+#
+# variable "zone_id" {
+#   default = "Z0345275C3S6UDSOR4CU"
+# }
+#
+# variable "components" {
+#   default = {
+#     frontend = { name = "frontend-dev", type = "t2.micro" }
+#     # mongodb = { name = "mongodb-dev", type = "t2.micro" }
+#     # catalogue = { name = "catalogue-dev", type = "t2.micro" }
+#     # redis = { name = "redis-dev", type = "t2.micro" }
+#     # user = { name = "user-dev", type = "t2.micro" }
+#     # cart = { name = "cart-dev", type = "t2.micro" }
+#     # mysql = { name = "mysql-dev", type = "t2.micro" }
+#     # shipping = { name = "shipping-dev", type = "t2.micro" }
+#     # rabbitmq = { name = "rabbitmq-dev", type = "t2.micro" }
+#     # payment = { name = "payment-dev", type = "t2.micro" }
+#   }
+# }
+
+resource "aws_instance" "frontend" {
+  ami = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  instance_type = "t2.micro"
 
   tags = {
-    Name = lookup(each.value, "name", null)
-  }
-}
-
-output "aws_instance" {
-  value = aws_instance.instance
-}
-
-resource "aws_route53_record" "record" {
-  for_each      = var.components
-  zone_id       = var.zone_id
-  name          = "${lookup(each.value, "name", null)}.vinithaws.online"
-  type          = "A"
-  ttl           = 30
-  records       = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
-}
-
-variable "ami" {
-  default = "ami-0b4f379183e5706b9"
-}
-
-
-variable "vpc_security_group_ids" {
-  default = ["sg-0ad8ec6873fafd140"]
-}
-
-variable "zone_id" {
-  default = "Z0345275C3S6UDSOR4CU"
-}
-
-variable "components" {
-  default = {
-    frontend = { name = "frontend-dev", type = "t2.micro" }
-    # mongodb = { name = "mongodb-dev", type = "t2.micro" }
-    # catalogue = { name = "catalogue-dev", type = "t2.micro" }
-    # redis = { name = "redis-dev", type = "t2.micro" }
-    # user = { name = "user-dev", type = "t2.micro" }
-    # cart = { name = "cart-dev", type = "t2.micro" }
-    # mysql = { name = "mysql-dev", type = "t2.micro" }
-    # shipping = { name = "shipping-dev", type = "t2.micro" }
-    # rabbitmq = { name = "rabbitmq-dev", type = "t2.micro" }
-    # payment = { name = "payment-dev", type = "t2.micro" }
+    Name = "frontend"
   }
 }
