95d7d0b7a4d1ff40fe84e801095531eb8d816c76
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 13:50:47 +0530

    test

--- 07-modules/local-module/main.tf
+++ 07-modules/local-module/main.tf
@@ -4,17 +4,17 @@ data "aws_ami" "ami" {
   owners           = ["973714476881"]
 }
 
-resource "aws_instance" "instance" {
-  count                   = length(var.name)
+resource "aws_instance" "instances" {
+  ami                     = var.ami
   vpc_security_group_ids  = var.vpc_security_group_ids
   instance_type           = var.instance_type
-  ami                     = data.aws_ami.ami.id
 
   tags = {
-    Name = element(var.name, count.index)
+    Name = var.name
   }
 }
 
-variable "name" {}
 variable "instance_type" {}
+variable "ami" {}
 variable "vpc_security_group_ids" {}
+variable "name" {}
--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -1,18 +1,27 @@
-module "test" {
+module "components" {
   source = "./local-module"
-  name   = var.name
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+  for_each                = var.components
+  instance_type           = var.instance_type
+  ami                     = var.ami
+  vpc_security_group_ids  = var.vpc_security_group_ids
+  name                    = each.key
 }
 
 
 
-variable "name" {
-  default = ["frontend-dev", "mongodb-dev", "catalogue-dev"]
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "vpc_security_group_ids" {
-  default = ["sg-0ad8ec6873fafd140"]
+variable "instance_type" {}
+variable "ami" {}
+variable "vpc_security_group_ids" {}
+variable "components" {}
+
+
+
+instance_type = "t2.micro"
+ami           = "ami-0b4f379183e5706b9"
+vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+
+components = {
+  frontend = {}
+  mongodb  = {}
+  catalogue = {}
 }
