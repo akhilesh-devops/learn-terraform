2b42954909af6cd5f0b1bb248360787033fe65ae
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 12:43:39 +0530

    test

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -3,28 +3,27 @@ variable "fruits" {
 }
 
 output "fruits" {
-  # value = var.fruits[4]
-  value = element(var.fruits, 10)
+  value = element(var.fruits, 150)
 }
 
-variable "fruit_details" {
-  default = {
-    apple = {
-      # price = 100
-      stock = 10
-    }
-    banana = {
-      price = 20
-      # stock = 5
-    }
-  }
-}
-
-output "orange_price" {
-  value = try(var.fruit_details["orange"].price, "null")
-}
-
-output "apple_price" {
-  # value = try(var.fruit_details["apple"].price, "null")
-  value = lookup(var.fruit_details["apple"], "price", "null")
-}
+# variable "fruit_details" {
+#   default = {
+#     apple = {
+#       # price = 100
+#       stock = 10
+#     }
+#     banana = {
+#       price = 20
+#       # stock = 5
+#     }
+#   }
+# }
+#
+# output "orange_price" {
+#   value = try(var.fruit_details["orange"].price, "null")
+# }
+#
+# output "apple_price" {
+#   # value = try(var.fruit_details["apple"].price, "null")
+#   value = lookup(var.fruit_details["apple"], "price", "null")
+# }
