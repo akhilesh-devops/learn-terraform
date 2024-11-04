42180b9270bc8097bd935603f19cdcf6b4121052
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 14:17:16 +0530

    test

--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -2,14 +2,13 @@ module "components" {
   source = "./local-module"
 
   for_each                = var.components
-  instance_type           = var.instance_type
+  instance_type           = lookup(each.value, "instance_type", null)
   vpc_security_group_ids  = var.vpc_security_group_ids
   name                    = lookup(each.value, "name", null)
 }
 
 
 
-variable "instance_type" {}
 
 variable "vpc_security_group_ids" {}
 
--- 07-modules/terraform.tfvars
+++ 07-modules/terraform.tfvars
@@ -1,6 +1,7 @@
 instance_type = "t2.micro"
 vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 components = {
-  frontend = { name = "frontend"}
-  mongodb  = { name = "mongodb"}
+  frontend = { name = "frontend", instance_type = "t2.micro" }
+  mongodb  = { name = "mongodb", instance_type = "t2.micro" }
+  catalogue = { name = "catalogue", instance_type = "t2.micro" }
 }
