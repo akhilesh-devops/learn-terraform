97e0f5470172a1f83dd72ed1061c778977e66814
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 23:26:35 +0530

    test

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,18 +1,31 @@
 variable "components" {
-  default = ["frontend", "catalogue", "redis"]
-}
-
-resource "aws_instance" "instances" {
-  count                  = length(var.components)
-  ami                    = var.ami
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
-  instance_type          = var.instance_type
-
-  tags = {
-    Name = element(var.components, count.index)
+  default = {
+    frontend = {
+      name = "frontend-dev"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name = "mongodb-dev"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name = "catalogue-dev"
+      instance_type = "t2.micro"
+    }
   }
 }
 
-variable "instance_type" {}
+resource "aws_instance" "instances" {
+  for_each               = var.components
+  ami                    = var.ami
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  instance_type          = lookup(each.value, "instance_type", null)
+
+  tags = {
+    Name = lookup(each.value, "name", null)
+  }
+}
+
+
 
 variable "ami" {}
--- 06-loops/for-each/terraform.tfvars
+++ 06-loops/for-each/terraform.tfvars
@@ -1,2 +1,2 @@
-instance_type = "t2.micro"
+
 ami = "ami-0b4f379183e5706b9"
