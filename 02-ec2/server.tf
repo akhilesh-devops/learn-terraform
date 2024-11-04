be805841db06b43278b6f1f8282f3f4d4821c4a1
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-04 22:40:46 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,17 +1,9 @@
-resource "aws_instance" "frontend" {
-  ami = "ami-0b4f379183e5706b9"
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+resource "aws_instance" "instances" {
+  ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
   tags = {
     Name = "frontend"
   }
-}
-
-resource "aws_route53_record" "record" {
-  zone_id       = "Z0345275C3S6UDSOR4CU"
-  name          = "frontend.vinithaws.online"
-  type          = "A"
-  ttl           = 30
-  records       = [aws_instance.frontend.private_ip]
 }
