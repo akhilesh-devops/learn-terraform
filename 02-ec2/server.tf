0f68552b37d52912b9ca8161be8acd52dfd59e6d
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-12 16:31:13 +0530

    test

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,23 +1,9 @@
 resource "aws_instance" "instances" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
   tags = {
     Name = "frontend"
   }
-}
-
-output "instances" {
-  value = aws_instance.instances
-}
-
-
-
-resource "aws_route53_record" "record" {
-  zone_id = "Z0345275C3S6UDSOR4CU"
-  name    = "frontend.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instances.private_ip]
 }
