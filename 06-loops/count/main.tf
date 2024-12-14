5e3f31355ed4d284d38efe8857fed0e2fa8f36fd
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 15:42:35 +0530

    test

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,32 +1,15 @@
-variable "components" {
-  default = ["catalogue", "user", "frontend", "mongodb"]
-}
-
-resource "aws_instance" "instance" {
-  count         = length(var.components)
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+resource "aws_instance" "instances" {
+  count                  = length(var.components)
+  ami                    = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  instance_type          = "t2.micro"
 
   tags = {
-    Name = element(var.components, count.index)
-    # Name = var.components[count.index]
+    Name = element(var.components["count.index"])
   }
 }
 
-variable "ami" {
-  default = "ami-0b4f379183e5706b9"
-}
 
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "vpc_security_group_ids" {
-  default = ["sg-0ad8ec6873fafd140"]
-}
-
-resource "aws_security_group" "allow-all" {
-  count = length(var.components)
-  name  = element(var.components, count.index)
+variable "components" {
+  default = ["frontend", "mongodb", "catalogue", "redis", "user", "cart"]
 }
