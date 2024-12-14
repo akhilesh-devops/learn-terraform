45ddcbe6d021c67be21420848f7026c7e0c96f91
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 15:16:45 +0530

    test

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -2,15 +2,44 @@ variable "components" {
   default = ["frontend", "catalogue", "mongodb", "user"]
 }
 
-resource "aws_instance" "instance" {
-  count                   = length(var.components)
-  instance_type           = "t2.micro"
-  ami                     = "ami-0b4f379183e5706b9"
-  vpc_security_group_ids  = ["sg-0ad8ec6873fafd140"]
+# resource "aws_instance" "instance" {
+#   count                   = length(var.components)
+#   instance_type           = "t2.micro"
+#   ami                     = "ami-0b4f379183e5706b9"
+#   vpc_security_group_ids  = ["sg-0ad8ec6873fafd140"]
+#
+#   tags = {
+#     Name = element(var.components, count.index)
+#   }
+# }
 
-  tags = {
-    Name = element(var.components, count.index)
+variable "fruits" {
+  default = {
+    apple = {
+      stock = 100
+      price = 50
+      sale  = true
+    }
+    orange = {
+      stock = 200
+      price = 100
+      sale  = true
+    }
+    grapes = {
+      stock = 300
+      price = 200
+      sale  = false
+    }
+    banana = {
+      stock = 400
+      price = 300
+      sale = true
+    }
   }
+}
+
+output "apple_stock" {
+  value = try(var.fruits["apple"].stock)
 }
 
 
@@ -18,33 +47,6 @@ resource "aws_instance" "instance" {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# variable "laptops" {
-#   default = {
-#     hp = {
-#       processor = "amd-ryzen"
-#       memory    = "10G"
-#       cpu       = 2
-#     }
-#     del = {
-#       processor = "intel"
-#       memory    = "5G"
-#       cpu       = 5
-#     }
-#   }
-# }
-#
 # output "hp_processor" {
 #   # value = try(var.laptops["hp"].processor)
 #   value = lookup(var.laptops["hp"], "processor")
