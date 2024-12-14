935eb97d2abf9e199f09e597f672b450537105b5
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 10:45:44 +0530

    test

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -4,9 +4,9 @@ variable "fruit_name" {
   default = "apple"
 }
 
-output "fruit_name" {
-  value = var.fruit_name
-}
+# output "fruit_name" {
+#   value = var.fruit_name
+# }
 
 ## List variable
 
@@ -14,19 +14,46 @@ variable "fruits" {
   default = ["apple", "grapes", "orange", "banana"]
 }
 
-output "fruit_1" {
-  value = element(var.fruits, 0)
+# output "fruit_1" {
+#   value = element(var.fruits, 0)
+# }
+#
+# output "fruit_2" {
+#   value = element(var.fruits, 1)
+# }
+#
+# output "fruit_3" {
+#   value = element(var.fruits, 2)
+# }
+#
+# output "fruit_4" {
+#   # value = element(var.fruits, 3)
+#   value = var.fruits[3]
+# }
+
+## Map variable
+
+variable "fruit_stock" {
+  default = {
+    apple  = 100
+    orange = 200
+    banana = 300
+    grapes = 400
+  }
 }
 
-output "fruit_2" {
-  value = element(var.fruits, 1)
+output "apple_stock" {
+  value = var.fruit_stock["apple"]
 }
 
-output "fruit_3" {
-  value = element(var.fruits, 2)
+output "orange_stock" {
+  value = var.fruit_stock["orange"]
 }
 
-output "fruit_4" {
-  # value = element(var.fruits, 3)
-  value = var.fruits[3]
+output "banana_stock" {
+  value = var.fruit_stock["banana"]
+}
+
+output "grapes_stock" {
+  value = var.fruit_stock["grapes"]
 }
