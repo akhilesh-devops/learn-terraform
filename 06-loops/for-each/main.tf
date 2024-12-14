a313483040a8d47ea077826f8d5d16196137c5c2
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 16:31:25 +0530

    test

---
+++ 06-loops/for-each/main.tf
@@ -0,0 +1,21 @@
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend-dev"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = "mongodb-dev"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t2.micro"
+    }
+  }
+}
+
+resource "aws_security_group" "allow-all" {
+  for_each = var.components
+  name     = lookup(each.value, "name", null)
+}
