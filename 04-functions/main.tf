92f0a66b6ea77885b51180b2511ca6c867aa3ac0
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 15:32:29 +0530

    test

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -2,16 +2,16 @@ variable "components" {
   default = ["frontend", "catalogue", "mongodb", "user"]
 }
 
-resource "aws_instance" "instance" {
-  count                   = length(var.components)
-  instance_type           = "t2.micro"
-  ami                     = "ami-0b4f379183e5706b9"
-  vpc_security_group_ids  = ["sg-0ad8ec6873fafd140"]
-
-  tags = {
-    Name = element(var.components, count.index)
-  }
-}
+# resource "aws_instance" "instance" {
+#   count                   = length(var.components)
+#   instance_type           = "t2.micro"
+#   ami                     = "ami-0b4f379183e5706b9"
+#   vpc_security_group_ids  = ["sg-0ad8ec6873fafd140"]
+#
+#   tags = {
+#     Name = element(var.components, count.index)
+#   }
+# }
 
 variable "fruits" {
   default = {
