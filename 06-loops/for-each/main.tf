b51a503094906025d2488455768b3f5d4127869f
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-14 16:40:11 +0530

    test

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -13,9 +13,27 @@ variable "components" {
       instance_type = "t2.micro"
     }
     catalogue = {
-      name          = "catalogue"
+      name          = "catalogue-dev"
       instance_type = "t2.micro"
     }
   }
 }
 
+variable "ami" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0ad8ec6873fafd140"]
+}
+
+resource "aws_instance" "instances" {
+  for_each               =    var.components
+  ami                    =    var.ami
+  vpc_security_group_ids =    var.vpc_security_group_ids
+  instance_type          =    each.value["instance_type"]
+
+  tags = {
+    Name = each.value["name"]
+  }
+}
